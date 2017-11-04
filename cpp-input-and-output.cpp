03915f63ab0beae4181ec260d9de7a3ff6616335
Muhammad Habib Jawady <33197357+hbibz-journey@users.noreply.github.com>
2017-11-04 16:59:58 +0100

    Create cpp-input-and-output.cpp

---
+++ cpp-input-and-output.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+// main function
+int main() {
+    // defining sum var and initializing it with zero
+    int sum = 0;
+    // inititalizing variables to contain the values
+    int x, y, z;
+    // collecting input from stdin
+    cin >> x >> y >> z;
+    // calculating the sum of values
+    sum = x + y + z;
+    // outputting the sum to stdout
+    cout << sum << endl;
+
+    return 0;
+}
